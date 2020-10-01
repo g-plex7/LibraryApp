13fbe78055245cb93a2afbc649a9ef8ec625eafb
g-plex7 <co.write@yahoo.com>
2020-10-01 20:25:52 +0700

    modify models and views

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,12 +1,6 @@
 class Borrowing < ApplicationRecord
     belongs_to: book 
     belongs_to: user, :optional => true
-    validate :validate_borrow
-
-    private  
-    def validate_borrow 
-        return if borrow.count <= 1
-        errors.add(:borrow, 'you cant borrow 1 book per user') 
-    end 
+    
 end
  
